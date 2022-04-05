46fd84dd8029934822c9db78a246656958183a2d
Lawrence Wagerfield <lawrence@projectone.io>
2022-04-05 11:32:43 +0100

    Make tests headless

--- tests/wdio.conf.ts
+++ tests/wdio.conf.ts
@@ -81,10 +81,20 @@ export const config: Options.Testrunner = {
       // maxInstances can get overwritten per capability. So if you have an in-house Selenium
       // grid with only 5 firefox instances available you can make sure that not more than
       // 5 instances get started at a time.
-      maxInstances: 5,
+      "maxInstances": 5,
       //
-      browserName: "chrome",
-      acceptInsecureCerts: true
+      "browserName": "chrome",
+      "goog:chromeOptions": {
+        args: [
+          "--headless",
+          "user-agent=...",
+          "--disable-gpu",
+          "--window-size=1440,735",
+          "--no-sandbox",
+          "--disable-dev-shm-usage"
+        ]
+      },
+      "acceptInsecureCerts": true
       // If outputDir is provided WebdriverIO can capture driver session logs
       // it is possible to configure which logTypes to include/exclude.
       // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
